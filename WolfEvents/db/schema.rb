02239453b6ad1da7af33be40c392249629f5efed
AniketShaktawat <aniketshaktawat0702@gmail.com>
2024-02-12 16:19:38 -0500

    changed reviews branch

--- WolfEvents/db/schema.rb
+++ WolfEvents/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_12_011608) do
+ActiveRecord::Schema.define(version: 2024_02_12_042259) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.date "date"
+    t.time "startTime"
+    t.time "endTime"
+    t.float "ticketPrice"
+    t.integer "seatsLeft"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
